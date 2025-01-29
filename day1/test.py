d51eb0f64198725767d3c67d6a22b0a357cb9598
shihab-7 <shihabshahriar543@gmail.com>
2025-01-29 23:05:53 +0600

    api key hide and test llm

--- day1/test.py
+++ day1/test.py
@@ -1,1 +1,9 @@
-print("check")
+from decouple import config
+from langchain_openai import OpenAI
+SECRECT_KEY = config("OPENAI_API_KEY")
+
+# print(SECRECT_KEY)
+# TO use llm openai
+llm = OpenAI("SECRECT_KEY")
+response = llm.invoke("What is your favourite number ?")
+print(response)
