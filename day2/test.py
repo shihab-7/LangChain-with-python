6ac901ae89e6c559d3b161d0f4a5baebfa6f4bd7
shihab-7 <shihabshahriar543@gmail.com>
2025-01-31 00:20:53 +0600

    solved complications using hugging face mistal ai text api

--- day2/test.py
+++ day2/test.py
@@ -1,19 +1,20 @@
 from decouple import config
 from langchain.prompts import PromptTemplate
-from langchain_huggingface import HuggingFaceEndpoint
+from huggingface_hub import InferenceClient
 
 SECRECT_KEY = config("HUGGINGFACE_API_KEY")
 # print(SECRECT_KEY)
 # val = input("Enter something...")
 
-template = "<s>[INST] write shrotly on </s>{question}[/INST]"
+template = "<s>[INST] write recipy </s>{question}[/INST]"
 prompt_template = PromptTemplate.from_template(template)
-formatted_template = prompt_template.format(question="who made you ?")
+formatted_template =prompt_template.format(question="rice,potatoes,milk,sugar")
 
 # print(formatted_template)
 repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
-llm = HuggingFaceEndpoint(repo_id=repo_id, huggingfacehub_api_token=SECRECT_KEY)
-response = llm.invoke(formatted_template)
+llm = InferenceClient(model=repo_id, token=SECRECT_KEY)
+response = llm.text_generation(formatted_template)
+for s in response:
+    print(s,end="")
 
-print(response)
 
