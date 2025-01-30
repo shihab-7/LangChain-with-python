66fc214e2d0c940bee5e9dcd604bc1e4645ee1f5
shihab-7 <shihabshahriar543@gmail.com>
2025-01-31 01:27:20 +0600

    solved all and got fully functional response

--- day2/test.py
+++ day2/test.py
@@ -4,11 +4,11 @@ from huggingface_hub import InferenceClient
 
 SECRECT_KEY = config("HUGGINGFACE_API_KEY")
 # print(SECRECT_KEY)
-# val = input("Enter something...")
+text = input("Enter something...")
 
-template = "<s>[INST] write recipy </s>{question}[/INST]"
+template = "<s>[INST] write briefly on </s>{question}[/INST]"
 prompt_template = PromptTemplate.from_template(template)
-formatted_template =prompt_template.format(question="rice,potatoes,milk,sugar")
+formatted_template =prompt_template.format(question=text)
 
 # print(formatted_template)
 repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
@@ -16,5 +16,4 @@ llm = InferenceClient(model=repo_id, token=SECRECT_KEY)
 response = llm.text_generation(formatted_template)
 for s in response:
     print(s,end="")
-
 
