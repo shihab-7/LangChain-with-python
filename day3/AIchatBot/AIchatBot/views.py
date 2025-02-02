42b200b4b52ef1d533f99a33d70767c7b273353f
shihab-7 <shihabshahriar543@gmail.com>
2025-02-02 23:04:04 +0600

    django template and context theory

--- day3/AIchatBot/AIchatBot/views.py
+++ day3/AIchatBot/AIchatBot/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("<h1>Hello world !</h1>")
+
+def test(request):
+    data={'name':"John", 'age':20}
+    return render(request, 'home.html',data)
